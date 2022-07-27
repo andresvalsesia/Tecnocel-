06252d346fda032502e82814a5a65286aed2a251
nicoviale <106348848+nicoviale@users.noreply.github.com>
2022-07-27 12:16:23 -0300

    corazon header

--- frontend/src/component/home/Header.jsx
+++ frontend/src/component/home/Header.jsx
@@ -15,32 +15,26 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import userActions from '../../redux/actions/userActions';
-import { useDispatch } from 'react-redux'
-import { useNavigate } from "react-router-dom"
+import {useDispatch} from 'react-redux'
+import {useNavigate} from "react-router-dom"
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-let pages = [
-  {
-    name: "Inicio",
-    to: "/"
-  },
-  {
-    name: "Nosotros",
-    to: "/about"
-  },
-  {
-    name: "Productos",
-    to: "/products"
-  },
-  {
-    name: "Politicas",
-    to: "/politicas"
-  },
-  {
-    name: "Contacto",
-    to: "/contact"
-  }]
+
+
+let pages=[
+  {name:"Inicio",
+   to:"/"},
+   {name:"Nosotros",
+   to:"/about"},
+   {name:"Productos",
+   to:"/products"},
+   {name:"Politicas",
+   to:"/politicas"},
+   {name:"Contacto",
+   to:"/contact"}]
 
 const Header = () => {
 
@@ -56,15 +50,16 @@ const Header = () => {
     setAnchorElUser(event.currentTarget);
   };
 
-  
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+
+  const navigate = useNavigate ()
+  const dispatch = useDispatch ()
 
 
-  const logOutSession = () => {
+  const logOutSession= () => {
     dispatch(userActions.signOutUser())
-    navigate()
-    setReload(!reload)
+    navigate("/")
+    window.location.reload()
+    
   }
 
 
@@ -76,7 +71,6 @@ const Header = () => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   }
-
 
 
   let user = useSelector(store => store.userReducer.user)
@@ -186,77 +180,77 @@ const Header = () => {
       </div>
       {/* Header Navbar */}
       <div className="navbar flex pz__10 space__beetween" ref={switcherTab}>
+      
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          
 
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-
-
-
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton
-
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon className="menu-despleg" />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'flex', md: 'none' },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <LinkRouter className="Linkrouter-short" to={page.to}>
-                      {page.name}
-                    </LinkRouter>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+            
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon className="menu-despleg" />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'flex', md: 'none' },
+              }}
+            >
               {pages.map((page) => (
-                <Button
-                  key={page.name}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  <LinkRouter className="Linkrouter" to={page.to}>
-                    {page.name}
-                  </LinkRouter>
-                </Button>
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                   <LinkRouter className="Linkrouter-short" to={page.to}>
+              {page.name}
+            </LinkRouter>
+                </MenuItem>
               ))}
-            </Box>
+            </Menu>
+          </Box>
+         
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+                  <Button
+                    key={page.name}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+              <LinkRouter className="Linkrouter" to={page.to}>
+              {page.name}
+            </LinkRouter>
+              </Button>
+            ))}
+          </Box>
 
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-
-                </IconButton>
-              </Tooltip>
-
-            </Box>
-          </Toolbar>
-        </Container>
-
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                
+              </IconButton>
+            </Tooltip>
+            
+          </Box>
+        </Toolbar>
+      </Container>
+    
         {/* <div
           className="navigation"
           style={{
@@ -292,16 +286,35 @@ const Header = () => {
             </LinkRouter>
           </ul>
         </div> */}
-
+        
 
         <div className="rightOption flex align__items__center">
 
-          {user && user.role == "admin" && <LinkRouter to="/admin">
-            <AdminPanelSettingsIcon className="panel" />
-          </LinkRouter>}
+        {user ?
+          <IconButton onClick={()=>/* likesOrDislikes */(user._id)} aria-label="add to favorites">
+            {/* {user?.likes.includes(user.user?.id) ? */}
+             {/*  <FavoriteIcon style={{ color: 'red' }} /> */}
+              :
+              <FavoriteBorderIcon />}
+            
+          </IconButton>
+          :
+          <IconButton /* onClick={noUser} */ aria-label="add to favorites">
+            <FavoriteBorderIcon />
+            
+          </IconButton>
+        }
 
 
 
+
+          
+          {user && user.role=="admin" &&  <LinkRouter  to="/admin">
+             <AdminPanelSettingsIcon className="panel"/> 
+            </LinkRouter>}
+            
+      
+         
           <div className="cart__items flex align__items__center">
             <div className="cart__items flex pointer relative">
               <LinkRouter to="/cart">
@@ -316,13 +329,13 @@ const Header = () => {
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                 </svg>
               </LinkRouter>
-
+             
             </div>
           </div>
           <Box sx={{ flexGrow: 0 }}>
           </Box><Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
                 {user ? <Avatar alt="photo" src={user.user?.photo} sx={{
                   width: '40px',
                   height: '40px',
@@ -333,21 +346,20 @@ const Header = () => {
                   backgroundColor: '#121212',
                   marginLeft: '15px',
                   textDecoration: 'none',
-                  borderRadius: '20px'
-                }} /> :
+                  borderRadius: '20px'}} /> :
                   <LinkRouter to="/login">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="25"
-                      height="25"
-                      marginleft="1px"
-                      fill="rgb(105,24,152)"
-                      className="userr"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                    </svg>
-                  </LinkRouter>}
+                  <svg 
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="25"
+                    height="25"
+                    marginleft="1px"
+                    fill="rgb(105,24,152)"
+                    className="userr"
+                    viewBox="0 0 16 16"
+                   >
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                  </svg>
+                </LinkRouter>}
               </IconButton>
             </Tooltip>
             <Menu
@@ -369,17 +381,17 @@ const Header = () => {
               {user ? (
                 <Box>
                   <LinkRouter to={`/profile/${user.user?.id}`}>
-                    {/*    <MenuItem sx={{'&:hover': {bgcolor: 'rgb(224,224,224)'}}} onClick={handleCloseUserMenu}>
+                 {/*    <MenuItem sx={{'&:hover': {bgcolor: 'rgb(224,224,224)'}}} onClick={handleCloseUserMenu}>
                       <Typography sx={{padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: 'rgb(2,0,3)'}}>{user.user?.name.charAt(0).toUpperCase()+user.user?.name.slice(1).toLowerCase()}</Typography>
                     </MenuItem> */}
                   </LinkRouter>
-                  <MenuItem sx={{ '&:hover': { bgcolor: '#141414c7' } }} handleReload={handleReload} onClick={handleCloseUserMenu}>
-                    <Typography sx={{ padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: '#88d317c7' }} handleReload={handleReload} onClick={logOutSession}>Sign Out</Typography>
+                  <MenuItem sx={{'&:hover': {bgcolor: '#141414c7'}}} handleReload={handleReload} onClick={handleCloseUserMenu}>
+                    <Typography sx={{padding: '2px', paddingLeft: '6px', paddingRight: '6px', color: '#88d317c7'}} handleReload={handleReload} onClick={logOutSession}>Sign Out</Typography>
                   </MenuItem>
                 </Box>
-              ) :
+              ) : 
                 <LinkRouter to="/login">
-                  {/*       <svg
+   {/*       <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="25"
                 height="25"
@@ -390,10 +402,10 @@ const Header = () => {
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
               </svg> */}
                 </LinkRouter>
-              }
+}
             </Menu>
           </Box>
-          {/*           <div className="user__account flex pointer">
+{/*           <div className="user__account flex pointer">
             <LinkRouter to="/login">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -410,7 +422,7 @@ const Header = () => {
         </div>
       </div>
     </div>
-
+    
 
   );
 }
